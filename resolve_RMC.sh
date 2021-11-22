50d7a24797a4333aa10c26b6527f95f918fdd7af
SAIKUMAR SRIGIRIRAJU <80637313+SSRIGIRIRAJU22@users.noreply.github.com>
2021-11-22 12:37:43 +0530

    Update resolve_RMC.sh

--- resolve_RMC.sh
+++ resolve_RMC.sh
@@ -102,6 +102,13 @@ do
     esac
 done
 
+if [[ "$#" -ne 0 ]]
+then
+        echo ""
+        usage
+        exit 1
+fi
+
 [[ "$MACHINE" == "" ]] && echo -e "\nOption: -m is missing.\n" && usage && exit 1
 [[ "$USER" == "" ]] && echo -e "\nOption: -u is missing.\n" && usage && exit 1
 [[ "$PASSWORD" == "" ]] && echo -e "\nOption: -p is missing.\n" && usage && exit 1
